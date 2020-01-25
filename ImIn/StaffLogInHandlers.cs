8416b35c5b1cdc55089cda411bf03301dfeb49cb
Cameron McNeill <cameron.mcneill24@hotmail.co.uk>
2020-01-25 15:21:47 +0000

    When employee clocks in, the database is updated to reflect that

--- ImIn/StaffLogInHandlers.cs
+++ ImIn/StaffLogInHandlers.cs
@@ -29,11 +29,18 @@ namespace ImIn
             accessDB.Start();
             accessDB.Join();
 
-            Cursor.Current = Cursors.Default;
 
             if (staff_id != "-1")
-                Console.WriteLine("User clocked in");
+            {
+                Thread updateTimeLog = new Thread(() => {
+                    LogClockIn(staff_id);
+                });
+                updateTimeLog.Start();
+                updateTimeLog.Join();
+            }
 
+
+            Cursor.Current = Cursors.Default;
 
             foreach (Control c in window.Controls)
             {
@@ -61,13 +68,28 @@ namespace ImIn
 
             string com_password = new PasswordHash().ComputeSha256Hash(password);
 
-            List<string>[] results = db.Select("select ID, Name, PIN from Employee where LocationID = '00000001'");
+            List<string>[] results = db.Select("select ID, Name, PIN from Employee"); // where LocationID = '00000001'");
 
             for (int i = 0; i < results[1].Count; i++)
                 if (results[2][i].Trim() == com_password)
                     return results[0][i];
 
             return "-1";
+        }
+
+
+        public void LogClockIn(string staff_id)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss");
+            string date = DateTime.Now.ToString("yyyy/MM/dd");
+            DBConnection db = new DBConnection();
+
+            List<string>[] results = db.Select("select ClockIn from TimeLog where EmployeeID = " + staff_id + " order by ClockTime desc, ClockDate desc");
+
+            bool in_out = !(bool.Parse(results[0][0]));
+
+            db.Insert("insert into TimeLog(EmployeeID, ClockIn, ClockTime, ClockDate) values (" + staff_id + ", " + in_out + ", '" + time + "', '" + date + "')");
+
         }
 
     }
